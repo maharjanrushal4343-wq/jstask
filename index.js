4657580ec14bddb8ca5b0bf4644346f2c57ee690
Rushal Maharjan <numb123jhiyo@gmail.com>
2026-05-13 12:07:00 +0545

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// if (12 % 2 == 0) {
+//   console.log("its a even number");
+// } else {
+//   console.log("not a even number");
+// }
+
+// let x = 11;
+
+// if (x > 0 && x % 2 == 0) {
+//   console.log("its is positive even number");
+// } else if (x > 0 && x % 2 !== 0) {
+//   console.log("Its positive is odd number");
+// } else if (x < 0 && x % 2 == 0) {
+//   console.log("its is a negative even number");
+// } else {
+//   console.log("its is negative and odd number");
+// }
+
+// for (i = 1; i <= 10 ; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+let i = 1
+while (i <= 10) { 
+  console.log(i * 2);
+  i++;
+}
